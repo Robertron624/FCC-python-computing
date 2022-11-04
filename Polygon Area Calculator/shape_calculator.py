df99a9612a382b35b4b8db68766cd52fe793b752
Roberto Ramirez <robert.624@hotmail.com>
2022-11-03 22:21:17 -0500

    finished shape calculator

--- Polygon Area Calculator/shape_calculator.py
+++ Polygon Area Calculator/shape_calculator.py
@@ -8,22 +8,22 @@ class Rectangle:
     def __str__(self) -> str:
         return f"Rectangle(width={self.width}, height={self.height})"
 
-    def set_width(self, width):
+    def set_width(self, width) -> None:
         self.width = width
 
-    def set_height(self, height):
+    def set_height(self, height) -> None:
         self.height = height
 
-    def get_area(self):
+    def get_area(self) -> int:
         return self.width * self.height
 
-    def get_perimeter(self):
+    def get_perimeter(self) -> int:
         return 2 * self.width + 2 * self.height
 
-    def get_diagonal(self):
+    def get_diagonal(self) -> int:
         return ((self.width ** 2 + self.height ** 2) ** 0.5)
 
-    def get_picture(self):
+    def get_picture(self) -> str:
 
         width = self.width
         height = self.height
@@ -40,8 +40,11 @@ class Rectangle:
 
         return picture
 
-    def get_amount_inside(self, Rectangle):
-        return ""
+    def get_amount_inside(self, Rectangle) -> int:
+        rectangle_area = Rectangle.get_area()
+        this_area = self.get_area()
+        
+        return int(this_area / rectangle_area)
 
 
 class Square(Rectangle):
@@ -54,23 +57,47 @@ class Square(Rectangle):
     def __str__(self) -> str:
         return f"Square(side={self.side})"
 
-    def set_side(self, side):
+    def set_side(self, side) -> None:
         self.side = side
         self.width = side
         self.height = side
 
+    def set_height(self, height) -> None:
+        self.height = height
+        self.width = height
+        self.side = height
 
-rect = Rectangle(10, 5)
-print(rect.get_area())
-rect.set_height(3)
-print(rect.get_perimeter())
-print(rect)
-print(rect.get_picture())
+    def set_width(self, width) -> None:
+        self.width = width
+        self.height = width
+        self.side = width
+
+    
+
+# TEST 1
+# rect = Rectangle(10, 5)
+# print(rect.get_area())
+# rect.set_height(3)
+# print(rect.get_perimeter())
+# print(rect)
+# print(rect.get_picture())
 
 
-sq = Square(9)
-print(sq.get_area())
-sq.set_side(4)
-print(sq.get_diagonal())
+# sq = Square(9)
+# print(sq.get_area())
+# sq.set_side(4)
+# print(sq.get_diagonal())
+# print(sq)
+# print(sq.get_picture())
+
+# rect.set_height(8)
+# rect.set_width(16)
+# print(rect.get_amount_inside(sq))
+
+
+# TEST 2
+
+sq = Square(10)
+
+sq.set_side(2)
 print(sq)
-print(sq.get_picture())
