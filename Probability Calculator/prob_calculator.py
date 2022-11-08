bc07d52f4385961eca2e1e1abbe8090088efd145
Roberto Ramirez <robert.624@hotmail.com>
2022-11-08 09:24:25 -0500

    experiment class finished

--- Probability Calculator/prob_calculator.py
+++ Probability Calculator/prob_calculator.py
@@ -14,24 +14,32 @@ class Hat:
                 temp.append(color)
         self.contents = temp
 
-    def draw(self, number_to_draw):
+    def draw(self, number_to_draw) -> list:
         drawed_balls = []
         if number_to_draw > len(self.contents):
-            all_balls = copy.deepcopy(self.contents)
-            self.contents = []
-            return all_balls
+            return self.contents
         for i in range(number_to_draw):
-            drawed_ball = self.contents.pop(i)
-            drawed_balls.append(drawed_ball)
-
+            ball = self.contents.pop(random.randrange(len(self.contents)))
+            drawed_balls.append(ball)
         return drawed_balls
 
     def get_contents(self):
         return self.contents
 
 
-def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    pass
+def experiment(hat: Hat, expected_balls: dict, num_balls_drawn: int, num_experiments: int) -> float:
+    count = 0
+    for _ in range(num_experiments):
+        copyhat = copy.deepcopy(hat)
+        temp_list = copyhat.draw(num_balls_drawn)
+        success = True
+        for key, value in expected_balls.items():
+            if temp_list.count(key) < value:
+                success = False
+                break
+        if success:
+            count += 1
+    return count / num_experiments
 
 
 x = Hat(red=2, blue=1)
